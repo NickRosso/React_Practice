21e6cdbfc8831190b3416a0ed238042d0c6858ef
unknown <nicholasrosso@comcast.net>
2018-12-28 16:56:44 -0600

    added sky,ground and ball SVG elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Canvas from './components/Canvas';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>We will create a game..</h1>
-      </div>
+      <Canvas />
     );
   }
+}
+
+App.propTypes = {
+
+  message: PropTypes.string.isRequired,
+
 }
 
 export default App;
